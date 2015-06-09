f50da1dc4e49185bbcb7ab44e4fc1601fc2b82bd
SoulMyke77 <bnardmickael@yahoo.fr>
2015-06-09 14:00:03 +0200

    attributs et propriétés

--- lecteurMediaWPF/BO/Video.cs
+++ lecteurMediaWPF/BO/Video.cs
@@ -8,5 +8,26 @@ namespace BO
 {
     public class Video
     {
+        #region ATTRIBUTS
+
+        private string nom;
+        private int duree;
+
+        #endregion
+
+        #region PROPRIETES
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public int Duree 
+        {
+            get { return duree; }
+            set { duree = value; }
+        }
+        #endregion
     }
 }
